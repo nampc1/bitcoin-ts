aae91186485be630dd3b31eb32867e4f299ed1a0
nampc <namphamc99@gmail.com>
2025-08-19 11:45:34 +0700

    feat: finite field & ecc

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,129 @@
 class FieldElement {
-  #num: number;
-  #prime: number;
+  private _num: bigint;
+  private _prime: bigint;
 
-  constructor(num: number, prime: number) {
+  constructor(num: bigint, prime: bigint) {
     if (num >= prime || num < 0) {
-      throw Error(`Num ${num} is not in field range 0 to ${prime}`);
+      throw new Error(`Num ${num} is not in field range 0 to ${prime}`);
     }
 
-    this.#num = num;
-    this.#prime = prime;
+    this._num = num;
+    this._prime = prime;
   }
 
   get num() {
-    return this.#num;
+    return this._num;
   }
 
   get prime() {
-    return this.#prime;
+    return this._prime;
   }
 
   eq(other: FieldElement): boolean {
-    return this.#prime === other.#prime && this.#num === other.#num;
+    return this._prime === other._prime && this._num === other._num;
   }
 
   add(other: FieldElement): FieldElement {
     if (this.prime !== other.prime) {
-      throw Error('Two elements are not in the same field');
+      throw new Error('Two elements are not in the same field');
     }
 
     return new FieldElement(
-      Utils.modulo((this.num + other.num), this.prime),
-      this.prime);
+      Utils.modulo(this.num + other.num, this.prime),
+      this.prime
+    );
   }
 
   sub(other: FieldElement): FieldElement {
     if (this.prime !== other.prime) {
-      throw Error('Two elements are not in the same field');
+      throw new Error('Two elements are not in the same field');
     }
 
     return new FieldElement(
-      Utils.modulo((this.num - other.num), this.prime),
-      this.prime);
+      Utils.modulo(this.num - other.num, this.prime),
+      this.prime
+    );
   }
 
   mul(other: FieldElement): FieldElement {
     if (this.prime !== other.prime) {
-      throw Error('Two elements are not in the same field');
+      throw new Error('Two elements are not in the same field');
     }
 
     return new FieldElement(
-      Utils.modulo((this.num * other.num), this.prime),
-      this.prime);
+      Utils.modulo(this.num * other.num, this.prime),
+      this.prime
+    );
   }
 
-  pow(exponent: number): FieldElement {
+  pow(exponent: bigint): FieldElement {
+    const modifiedExponent = Utils.modulo(exponent, this.prime - 1n); // in case exponent is negative -> Fermat's theorem
+
     return new FieldElement(
-      Utils.modulo((this.num ** exponent), this.prime),
-      this.prime);
+      Utils.modulo(this.num ** modifiedExponent, this.prime),
+      this.prime
+    );
   }
 
+  div(other: FieldElement) {
+    if (this.prime !== other.prime) {
+      throw new Error('Two elements are not in the same field');
+    }
 
+    return new FieldElement(
+      Utils.modulo(this.num * other.num ** (this.prime - 2n), this.prime), // Fermat's theorem
+      this.prime
+    );
+  }
 }
 
 class Utils {
-  static modulo(n: number, d: number) {
-    if (d === 0) {
+  static modulo(n: bigint, d: bigint) {
+    if (d === 0n) {
       return n;
     }
-    if (d < 0) {
-      return NaN;
+
+    if (d < 0n) {
+      throw new Error('d must be greater than 0');
     }
-    return (n % d + d) % d;
+
+    return ((n % d) + d) % d;
+  }
+}
+
+class Point {
+  private x: bigint;
+  private y: bigint;
+  private a: bigint;
+  private b: bigint;
+
+  constructor(x: bigint, y: bigint, a: bigint, b: bigint) {
+    if (y ** 2n !== x ** 3n + a * x + b) {
+      throw new Error('Point is not on the curve');
+    }
+
+    this.a = a;
+    this.b = b;
+    this.x = x;
+    this.y = y;
+  }
+
+  eq(other: Point) {
+    return (
+      this.x === other.x &&
+      this.y === other.y &&
+      this.a === other.a &&
+      this.b === other.b
+    );
   }
 }
 
 function main() {
-  const test = new FieldElement(1, 2);
-  const test1 = Utils.modulo(-27, 13);
-  console.log(test1);
+  const test = new FieldElement(7n, 13n);
+  const test1 = new FieldElement(2n, 14n);
+  const result = test.pow(-3n);
+  console.log(result);
+  console.log(test.div(test1));
 }
 
 main();
