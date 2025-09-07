cb0799a3e10014af32c6dd3e00cfe9fd8c3c3f04
nampc <namphamc99@gmail.com>
2025-09-07 12:19:42 +0700

    feat: elliptic curve over finite field

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,26 @@
 export class FieldElement {
-  readonly _num: bigint;
-  readonly _prime: bigint;
+  readonly num: bigint;
+  readonly prime: bigint;
 
   constructor(num: bigint, prime: bigint) {
     if (num >= prime || num < 0) {
       throw new Error(`Num ${num} is not in field range 0 to ${prime}`);
     }
 
-    this._num = num;
-    this._prime = prime;
-  }
-
-  get num() {
-    return this._num;
-  }
-
-  get prime() {
-    return this._prime;
+    this.num = num;
+    this.prime = prime;
   }
 
   eq(other: FieldElement): boolean {
-    return this._prime === other._prime && this._num === other._num;
+    return this.prime === other.prime && this.num === other.num;
+  }
+
+  eqInt(other: bigint): boolean {
+    return this.num === other;
   }
 
   add(other: FieldElement): FieldElement {
-    if (this.prime !== other.prime) {
-      throw new Error('Two elements are not in the same field');
-    }
+    this.checkSameField(other);
 
     return new FieldElement(
       Utils.modulo(this.num + other.num, this.prime),
@@ -34,10 +28,15 @@ export class FieldElement {
     );
   }
 
+  addInt(other: bigint): FieldElement {
+    return new FieldElement(
+      Utils.modulo(this.num + other, this.prime),
+      this.prime
+    );
+  }
+
   sub(other: FieldElement): FieldElement {
-    if (this.prime !== other.prime) {
-      throw new Error('Two elements are not in the same field');
-    }
+    this.checkSameField(other);
 
     return new FieldElement(
       Utils.modulo(this.num - other.num, this.prime),
@@ -45,13 +44,25 @@ export class FieldElement {
     );
   }
 
+  subInt(other: bigint): FieldElement {
+    return new FieldElement(
+      Utils.modulo(this.num - other, this.prime),
+      this.prime
+    );
+  }
+
   mul(other: FieldElement): FieldElement {
-    if (this.prime !== other.prime) {
-      throw new Error('Two elements are not in the same field');
-    }
+    this.checkSameField(other);
 
     return new FieldElement(
       Utils.modulo(this.num * other.num, this.prime),
+      this.prime
+    );
+  }
+
+  mulInt(other: bigint): FieldElement {
+    return new FieldElement(
+      Utils.modulo(this.num * other, this.prime),
       this.prime
     );
   }
@@ -65,15 +76,34 @@ export class FieldElement {
     );
   }
 
-  div(other: FieldElement) {
-    if (this.prime !== other.prime) {
-      throw new Error('Two elements are not in the same field');
+  div(other: FieldElement): FieldElement {
+    this.checkSameField(other);
+
+    if (other.num === 0n) {
+      throw new Error('Division by zero');
     }
 
     return new FieldElement(
       Utils.modulo(this.num * other.num ** (this.prime - 2n), this.prime), // Fermat's theorem
       this.prime
     );
+  }
+
+  divInt(other: bigint): FieldElement {
+    if (other === 0n) {
+      throw new Error('Division by zero');
+    }
+
+    return new FieldElement(
+      Utils.modulo(this.num * other ** (this.prime - 2n), this.prime), // Fermat's theorem
+      this.prime
+    );
+  }
+
+  private checkSameField(other: FieldElement) {
+    if (this.prime !== other.prime) {
+      throw new Error('Two elements are not in the same field');
+    }
   }
 }
 
@@ -88,62 +118,47 @@ class Utils {
 }
 
 export class Point {
-  readonly _x: bigint | null;
-  readonly _y: bigint | null;
-  readonly _a: bigint;
-  readonly _b: bigint;
-  readonly _isAtInfinity: boolean;
+  readonly x: FieldElement | null;
+  readonly y: FieldElement | null;
+  readonly a: FieldElement;
+  readonly b: FieldElement;
+  readonly isAtInfinity: boolean;
 
-  private constructor(x: bigint | null, y: bigint | null, a: bigint, b: bigint) {
+  private constructor(x: FieldElement | null, y: FieldElement | null, a: FieldElement, b: FieldElement) {
     if (!Point.isOnCurve(x, y, a, b)) {
       throw new Error('Invalid point')
     }
 
-    this._isAtInfinity = x === null && y === null;
-    this._a = a;
-    this._b = b;
-    this._x = x;
-    this._y = y;
+    this.isAtInfinity = x === null && y === null;
+    this.a = a;
+    this.b = b;
+    this.x = x;
+    this.y = y;
   }
 
-  public static getPointAtInfinity(a: bigint, b: bigint) {
+  public static getPointAtInfinity(a: FieldElement, b: FieldElement) {
     return new Point(null, null, a, b);
   }
 
-  public static new(x: bigint, y: bigint, a: bigint, b: bigint) {
-    return new Point(x, y, a, b);
+  public static new(x: bigint, y: bigint, a: bigint, b: bigint, prime: bigint) { // pass prime so we don't have to check x, y, a, b are in the same field
+    const aElement = new FieldElement(a, prime);
+    const bElement = new FieldElement(b, prime);
+    const xElement = new FieldElement(x, prime);
+    const yElement = new FieldElement(y, prime);
+
+    return new Point(xElement, yElement, aElement, bElement);
   }
 
-  get isAtInfinity() {
-    return this._isAtInfinity;
-  }
-
-  get x() {
-    return this._x;
-  }
-
-  get y() {
-    return this._y;
-  }
-
-  get a() {
-    return this._a;
-  }
-
-  get b() {
-    return this._b;
-  }
-
-  private static isOnCurve(x: bigint | null, y: bigint | null, a: bigint, b: bigint): boolean {
+  private static isOnCurve(x: FieldElement | null, y: FieldElement | null, a: FieldElement, b: FieldElement): boolean {
     if (x === null && y === null) {
       return true;
     }
 
     if (x === null || y === null) {
-      throw new Error('Point must have x and y');
+      throw new Error('Point cannot have x or y be null');
     }
 
-    if (y ** 2n === x ** 3n + a * x + b) {
+    if (y.pow(2n).eq(x.pow(3n).add(x.mul(a)).add(b))) {
       return true;
     }
     
@@ -151,67 +166,69 @@ export class Point {
   }
 
   eq(other: Point) {
+    if (this.isAtInfinity && other.isAtInfinity) {
+      return true;
+    }
+
+    if (this.isAtInfinity || other.isAtInfinity) {
+      return false;
+    }
+
     return (
-      this.x === other.x &&
-      this.y === other.y &&
-      this.a === other.a &&
-      this.b === other.b
+      this.x?.eq(other.x!) &&
+      this.y?.eq(other.y!) &&
+      this.a.eq(other.a) &&
+      this.b.eq(other.b)
     );
   }
 
-  add(other: Point) {
-    if (!(this.a === other.a && this.b === other.b)) {
+  add(other: Point): Point {
+    if (!this.a.eq(other.a) || !this.b.eq(other.b)) {
       throw new Error('Cannot add 2 points not on the same curve');
     }
 
-    // addition with identity
+    // case 1: self + infinity = self
     if (other.isAtInfinity) {
-      return new Point(this.x, this.y, this.a, this.b);
+      return this;
     }
 
+    // case 2: infinity + other = other
     if (this.isAtInfinity) {
-      return new Point(other.x, other.y, other.a, other.b);
+      return other;
     }
 
-    // point doubling
+    // now, neither point is at infinity.
+
+    // case 3: self.x == other.x and self.y == -other.y
+    // self is the inverse of other, so the result is infinity.
+    // this check also prevents division by zero in the chord method.
+    if (this.x!.eq(other.x!) && this.y!.add(other.y!).eqInt(0n)) {
+      return Point.getPointAtInfinity(this.a, this.b);
+    }
+
+    // case 4: self == other (point doubling)
     if (this.eq(other)) {
       // the tangent line at point is vertical
-      if (this.y === 0n) {
+      if (this.y!.num === 0n) {
         return Point.getPointAtInfinity(this.a, this.b);
       }
 
       // the tangent line intersects the curve at point -2P
-      const slope = (3n * this.x! ** 2n + this.a!) / (2n * this.y!)
-      const x = slope ** 2n - 2n * this.x!;
-      const y = slope * (this.x! - x) - this.y!;
+      // slope = (3 * x1^2 + a) / (2 * y1)
+      const slope = this.x!.pow(2n).mulInt(3n).add(this.a).div(this.y!.mulInt(2n));
+      // x3 = slope^2 - 2*x1
+      const x = slope.pow(2n).sub(this.x!.mulInt(2n));
+      // y3 = slope * (x1 - x3) - y1
+      const y = slope.mul(this.x!.sub(x)).sub(this.y!);
 
       return new Point(x, y, this.a, this.b);
     }
 
-    // addition of inverses
-    if (this.x === other.x && this.y !== other.y) {
-      return Point.getPointAtInfinity(this.a, this.b);
-    }
-
-    // chord method
-    const slope = (this.y! - other.y!) / (this.x! - other.x!);
-    const x = slope ** 2n - this.x! - other.x!;
-    const y = slope * (this.x! - x) - this.y!;
+    // Case 5: self.x != other.x (chord method)
+    const slope = this.y!.sub(other.y!).div(this.x!.sub(other.x!));
+    const x = slope.pow(2n).sub(this.x!).sub(other.x!);
+    const y = slope.mul(this.x!.sub(x)).sub(this.y!);
 
     return new Point(x, y, this.a, this.b);
   }
 }
-
-function main() {
-  // const test = new FieldElement(7n, 13n);
-  // const test1 = new FieldElement(2n, 14n);
-  // const result = test.pow(-3n);
-  // console.log(result);
-  // console.log(test.div(test1));
-
-  // const point = Point.getPointAtInfinity();
-
-  // console.log(point.isAtInfinity);
-}
-
-main();
